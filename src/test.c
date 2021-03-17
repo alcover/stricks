70d88835d424e9d0bdce7796eca1f6d1a3de8d89
alcover <francois@alcover.fr>
2021-03-17 21:59:10 +0100

    cleaner test

--- src/test.c
+++ src/test.c
@@ -10,64 +10,21 @@
 #include "log.h"
 #include "util.c"
 
-#define X16(s) #s #s #s #s #s #s #s #s #s #s #s #s #s #s #s #s 
-#define BIG X16(aaaabbbbccccdddd)
-#define biglen 256
-const char* big = BIG;
-const char* huge = BIG BIG BIG BIG BIG BIG BIG BIG BIG BIG BIG BIG BIG BIG BIG BIG;
+#define x4(s) s s s s
 
+#define s8 "[8.....]"
+#define s32 x4(s8)
+#define s256 x4(s32) x4(s32)
 
 int main() {
 
-int a = max(2,1+3);
-LOGVI(a);
-// unsigned int count;
-// stx_t* results = stx_split("baaaad", "aa", &count);
+	stx_t s;
 
-// for (int i = 0; i < count; ++i) {
-//     printf("%s\n", results[i]);
-// }
+	s = stx_from("abc", 0);
+	stx_show(s);
 
-	// stx_t s = stx_from("foo, bar", 0);
-	// unsigned cnt = 0;
-
-	// stx_t* list = stx_split(s, ", ", &cnt);
-
-	// for (int i = 0; i < cnt; ++i) {
-	//     // printf("%s\n", list[i]);
-	//     stx_show(list[i]);
-	// }
-	 
-	// {
-	// 	stx_t s = stx_new(6);
-	// 	stx_show(s);
-	// 	stx_append_count(s, "foo", 0);
-	// 	stx_show(s); 
-	// 	printf("%zu %zu\n", stx_cap(s), stx_len(s));
-	// }
-
-	// {
-	// 	stx_t s = stx_new(300);
-	// 	stx_show(s);
-	// 	stx_append_count_alloc(&s, "foo", 0);
-	// 	stx_show(s); 
-	// 	printf("%zu %zu\n", stx_cap(s), stx_len(s));
-	// }
-
-	// {
-	// 	stx_t s = stx_from("foo");
-	// 	stx_show(s);
-	// 	stx_resize(&s, 300);
-	// 	stx_show(s); 
-	// 	printf("%zu %zu\n", stx_cap(s), stx_len(s));
-	// }
-	// {
-	// 	stx_t s = stx_new(3);
-	// 	stx_show(s);
-	// 	stx_append_count_alloc(&s, big, 0);
-	// 	stx_show(s); 
-	// 	printf("%zu %zu\n", stx_cap(s), stx_len(s));
-	// }
+	s = stx_from(s256, 0);
+	stx_show(s);
 
 
     return 0;
