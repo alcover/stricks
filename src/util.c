e9882e66aa92ea51e8fdf4eb3e7cfdfdfcfb5a9c
alcover <francois@alcover.fr>
2021-03-21 15:05:23 +0100

    prune

--- src/util.c
+++ src/util.c
@@ -1,4 +1,4 @@
- #define max(a,b) ({ \
+#define max(a,b) ({ \
 __typeof__ (a) _a = (a); \
 __typeof__ (b) _b = (b); \
 _a > _b ? _a : _b; })
@@ -16,50 +16,21 @@ strnlen (const char *s, const size_t n)
 static inline size_t
 str_count (const char *str, const char* tok)
 {
-	if (!str||!tok) return 0;
+    if (!str||!tok) return 0;
 
-	const size_t toklen = strlen(tok);
-	if (!toklen) return 0;
-	
-	size_t cnt = 0;
-	
-	while ((str = strstr(str,tok))) {
-		++cnt;
-		str += toklen;
-	}
-
-	return cnt;
-}
-
-
-static inline void 
-str_split (const char *str, const char* sep, 
-	void(*callback)(const char* tok, size_t len, void* ctx), 
-	void *ctx)
-{
-	if (!str||!sep) return;
-
-    const size_t seplen = strlen(sep);
-
-    if (!seplen) {
-    	callback(str, strlen(str), ctx); // ?
-    	return;
+    const size_t toklen = strlen(tok);
+    if (!toklen) return 0;
+    
+    size_t cnt = 0;
+    
+    while ((str = strstr(str,tok))) {
+        ++cnt;
+        str += toklen;
     }
 
-    const char *beg = str, *end = str;
-
-    while (*end) {
-        if (!strncmp (end, sep, seplen)) {
-            callback (beg, end - beg, ctx);
-            end += seplen;
-            beg = end;
-        } else {
-        	++end;
-        }
-    }
-
-    callback (beg, end - beg, ctx); // last part
+    return cnt;
 }
+
 
 static char*
 rand_str (size_t len)
