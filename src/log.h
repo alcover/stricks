1daad41ab0dbc525cb7af42cd256407cb158b6e3
alcover <francois@alcover.fr>
2021-03-21 14:52:30 +0100

    clean

--- src/log.h
+++ src/log.h
@@ -1,4 +1,5 @@
-#define LOG(s)		do{ printf("%s\n", s); fflush(stdout);} while(0)
+#define LOG(...)	do{ printf(__VA_ARGS__); printf("\n"); fflush(stdout);} while(0)
+#define LOGI(i)		do{ printf("%d\n", (int)i); fflush(stdout);} while(0)
 #define LOGVS(id)   do{ printf("%s : '%s'\n", #id, id);     fflush(stdout); } while(0)
 #define LOGVI(id)   do{ printf("%s : %d\n", #id, (int)id);  fflush(stdout); } while(0)
 
