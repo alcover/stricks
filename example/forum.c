10508770ad8fa1b61ba3b9248bc2014fc95abe25
alcover <francois@alcover.fr>
2021-03-22 16:15:32 +0100

    new forum example: +split

--- example/forum.c
+++ example/forum.c
@@ -1,7 +1,3 @@
-/*
-    Example usage.
-*/
-
 #include <stdio.h>
 #include <stdlib.h>
 #include <stdint.h>
@@ -10,86 +6,76 @@
 #include "../src/stx.h"
 #include "../src/log.h"
 
-#define DB "posts.csv"
+#define DB_PATH "posts.csv"
 #define PAGE_SZ 128
-#define POST_FMT "■ %s (%d points) %s\n"
+#define POST_FMT "%s (+%d) %s\n"
 
-int pagenum = 1;
+int pagen = 1;
 
 void send(stx_t page) 
 { 
-	printf ("---- PAGE %d [%zu/%d bytes] ----\n\n%s\n", 
-        pagenum++, stx_len(page), PAGE_SZ, page); 
+	printf ("--- PAGE %d (%zu/%zu bytes) ---\n\n%s\n", pagen++, stx_len(page), PAGE_SZ, page); 
 }
 
 int main()
 {
-	stx_t content = stx_load(DB);
+	stx_t page = stx_new(PAGE_SZ);
+	stx_t db_text = stx_load(DB_PATH);
+    // LOG(db_text);
 
-	if (!content) {
-        ERR ("Failed to load db file %s\n", DB);
+	if (!db_text) {
+        ERR ("Failed to load db file %s\n", DB_PATH);
         exit(EXIT_FAILURE);
     }
     
-    size_t content_len = stx_len(content);
-    LOG ("content length : %zu", content_len);
-
+    const size_t db_len = stx_len(db_text);
     int nrows;
-    stx_t *rows = stx_split(content, content_len, "\n", &nrows);
-    LOG ("rows count : %d", nrows);
-    puts("");
+    stx_t *rows = stx_split(db_text, db_len, "\n", &nrows);
+    
+    LOG ("Welcome to Stricky's forum !");
+    LOG ("db : %zu bytes, rows : %d\n", db_len, nrows);
+
+    // puts("");
 
     stx_t row;
 
     while ((row = *rows++)) {
 
-        if (!stx_len(row)) continue;
-        LOGVS(row);
-        int ncols;
+        if (!stx_len(row)) break;
+        // LOGVS(row);
+        int ncols; 
+        //free?
         stx_t *cols = stx_split(row, stx_len(row), ",", &ncols);
+        
         int votes = atoi(cols[0]);
         stx_t user = cols[1];
         stx_t text = cols[2];
-        LOGVI(votes);
-        LOGVS(user);
-        LOGVS(text);
-        puts("");
+        
+        // LOGVI(votes);
+        // LOGVS(user);
+        // LOGVS(text);
+
+        // format and append row
+        int appended = stx_catf (page, POST_FMT, user, votes, text); 
+        // LOGVI(appended);
+        // stx_show(page);
+
+        if (appended <= 0) {
+            // LOG("cut");
+        	// post won't fit, so flush page 
+        	send(page);
+        	// and re-add post.
+        	stx_reset(page);
+            // stx_show(page);
+        	stx_catf (page, POST_FMT, user, votes, text);
+        }
+        // puts("");
     }
 
+	// page not empty, send it.
+	if (stx_len(page)) send(page);
 
-	// stx_t page = stx_new(PAGE_SZ);
-	// char row[100];
-
- //    puts ("Welcome to Stricky's forum !\n");
-   
- //    while (fgets(row, sizeof row, db) != NULL) 
- //    {
- //    	int votes;
- //    	char user[20];
- //        char text[100];
-
- //        // parse row
- //        int match = sscanf (row, "%d,%[^,],%[^\n]", &votes, user, text);
- //        LOGVS(text);
-
- //        // format and append row
- //        int appended = stx_catf (page, POST_FMT, user, votes, text); 
- //        LOGVI(appended);
- //        stx_show(page);
-
- //        if (appended <= 0) {
- //        	// post won't fit, so flush page 
- //        	send(page);
- //        	// and re-add post.
- //        	stx_reset(page);
- //        	stx_catf (page, POST_FMT, user, votes, text);
- //        }
- //    }
-
-	// // page not empty, send it.
-	// if (stx_len(page)) send(page);
-
- //    stx_free(page);
+    stx_free(page);
 	
 	return 0;
 } 
