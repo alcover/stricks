feb5373ee7c78829945b397cd21d7f557c98966d
alcover <francois@alcover.fr>
2021-03-24 22:16:56 +0100

    clean

--- example/forum.c
+++ example/forum.c
@@ -8,7 +8,7 @@
 
 #define DB_PATH "posts.csv"
 #define PAGE_SZ 128
-#define POST_FMT "%s (+%d) %s\n"
+#define POST_FMT "%s \t (+%d) %s\n"
 
 int pagen = 1;
 
@@ -21,7 +21,6 @@ int main()
 {
 	stx_t page = stx_new(PAGE_SZ);
 	stx_t db_text = stx_load(DB_PATH);
-    // LOG(db_text);
 
 	if (!db_text) {
         ERR ("Failed to load db file %s\n", DB_PATH);
@@ -31,45 +30,31 @@ int main()
     const size_t db_len = stx_len(db_text);
     int nrows;
     stx_t *rows = stx_split(db_text, db_len, "\n", &nrows);
+    stx_t row;
     
     LOG ("Welcome to Stricky's forum !");
-    LOG ("db : %zu bytes, rows : %d\n", db_len, nrows);
-
-    // puts("");
-
-    stx_t row;
+    LOG ("db : %zu bytes, %d rows\n", db_len, nrows-1);
 
     while ((row = *rows++)) {
 
         if (!stx_len(row)) break;
-        // LOGVS(row);
+        
         int ncols; 
-        //free?
+        //todo free
         stx_t *cols = stx_split(row, stx_len(row), ",", &ncols);
         
         int votes = atoi(cols[0]);
         stx_t user = cols[1];
         stx_t text = cols[2];
         
-        // LOGVI(votes);
-        // LOGVS(user);
-        // LOGVS(text);
-
-        // format and append row
         int appended = stx_catf (page, POST_FMT, user, votes, text); 
-        // LOGVI(appended);
-        // stx_show(page);
 
+       	// post too long, so flush page, reset and re-add post 
         if (appended <= 0) {
-            // LOG("cut");
-        	// post won't fit, so flush page 
         	send(page);
-        	// and re-add post.
         	stx_reset(page);
-            // stx_show(page);
         	stx_catf (page, POST_FMT, user, votes, text);
         }
-        // puts("");
     }
 
 	// page not empty, send it.
